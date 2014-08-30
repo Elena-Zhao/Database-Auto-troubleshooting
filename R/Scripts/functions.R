054b6203f3efae044e861b4d6be061718c867a5d
Elena Zhao <ElenaZhao513@gmail.com>
2014-08-30 20:27:54 +0800

    Revert "V1.1 realtimeAnomalyDetect"
    This reverts commit eb2660ef5823b366eadf31d179ad4b75938275b3.

--- R/Scripts/functions.R
+++ R/Scripts/functions.R
@@ -1,32 +1,19 @@
-##########################################################
-## Aim:
-## 1. real time
-## 2. multi-metrics -- distributed
-## 3. interface -- dbinabox
-## 4. accuracy--reduce false positive
-## 5. test with real database incidents
-###########################################################
+rawData<-read.table("/Users/Elena/Desktop/Auto-Troubleshooting/R/DATA/mpdb.oracle.BGet.STATE.10s.txt", header=TRUE, sep = "")
 
-rawData<-read.table("/Users/Elena/Desktop/Auto-Troubleshooting/real session/mpdb.oracle.Calls.AVG.60s.txt", header=TRUE, sep = "")
-baseData # should be vectors
-totalCount <- length(baseData)
 # Anomalies detection using time series analysis
-detection <- function(metrics = rawData, forecastData = NULL, step = 8, windowSize = 500, reportBaseline = 5) {
+detection <- function(metrics = rawData, step = 8, windowSize = 500, reportBaseline = 5) {
     #Create time series for all metrics values
-    metrics<-aggt(metrics, 5)
     metricsts <-ts(metrics$VALUE)
-   
-    count <- 0 #the number of values already used, corresponding to "metrics"
-    
+    #Count: the number of values already used
+    count <- 150
+    #windowSize <- 500
     #Initialize dataset for pattern analysis
-    if(length(forecastData) == 0)){
-        forecastData <- metricsts[1:150]
-        count <- 150
-    }
-    
-    outliers <- NULL   #outliers detected
-    
-    forecasts <- NULL   #all the forecasted values: mean
+    forecastData <- metricsts[1:count]
+    #Anomalies detected
+    outliers <- NULL
+    #all the forecasted values: mean
+    forecasts <- NULL
+    #forecastsResiduals <- NULL
     anomalies <- NULL
     forecastStep <- step
     #the upper and lower forescasts intervals: 80% and 95%
@@ -36,15 +23,23 @@ detection <- function(metrics = rawData, forecastData = NULL, step = 8, windowSi
     forecastsLower2 <- NULL
     forecastsResiduals <- NULL
     
+    #-----------------------------------------------------------------------
+    # Tranvers the entire time series with step forecastStep
+    # method: holt-winters, one dimention, needs to be further optimized
+    # Implementation philosophy: each time use the forecastData to predict the values and then compare them with the actual values, and alarts when the actual values fall out of the predicted bounds
+    # Change the anomalies to the appropriate values for the future predictions' accuracy
+    # and add all the values to the forecastData after comparasion
+    #-----------------------------------------------------------------------
+    
     while(count<length(metricsts)){
         if(length(forecastData) > windowSize){
-           forecastData <- forecastData[(length(forecastData)-windowSize+1): length(forecastData)]
+           forecastData <- forecastData[201: length(forecastData)]
         }
-        
-        #KEY:forecast...
         metricsforecast<-HoltWinters(forecastData, beta=FALSE, gamma=FALSE)
         metricsforecast2<-forecast.HoltWinters(metricsforecast, h = forecastStep, level = c(95, 99.5))
         
+        #arima <- auto.arima(forecastData)
+        #metricsforecast2 <- forecast.Arima(arima, h = forecastStep, level = c(95, 99.5))
         forecasts <- c(forecasts, metricsforecast2$mean)
         forecastsResiduals <- c(forecastsResiduals, metricsforecast2$residuals)
         
@@ -56,33 +51,27 @@ detection <- function(metrics = rawData, forecastData = NULL, step = 8, windowSi
         data <- metricsts[(count+1):(count+forecastStep)]
         #printOutliers(data, metricsforecast2$upper[, 2], metricsforecast2$lower[, 2])
         tmp <- which(data > metricsforecast2$upper[, 2] | data < metricsforecast2$lower[, 2])
-        
-        #异常的检测机制仅仅是靠预测，那么万一预测不准怎么办。。加入另一个种机制相结合！
         outliers <- c(outliers,(count + tmp))
-        #outliers adjustment，这里是不是可以改的合理一点
+        #outliers adjustment
         data[tmp] <- mean(data, trim = 0.4)
         forecastData <- c(forecastData, data)
-        assign("baseData", c(baseData, data))
         count <- count + forecastStep
-        assign("totalCount", totalCount + forecastStep)
     }
     
     anomalies <- findAnomalies(outliers, reportBaseline)
     anomalies[,1] <- outliers[anomalies[,1]]
+    residualSd <- sd(forecastsResiduals)
 
     #begin plotting
     plot(metricsts, type = "l", main = "Anomalies Detection")
-    lines(1, forecasts, type = "l", col = "red")
-    #lines(151:(150+length(forecasts)), forecasts, type = "l", col = "red")
-    #lines(151:(150+length(forecastsUpper)), forecastsUpper, type = "l", lty = "dashed", col = "blue")
-    #lines(151:(150+length(forecastsLower)), forecastsLower, type = "l", lty = "dashed", col = "blue")
-    #lines(151:(150+length(forecastsUpper2)), forecastsUpper2, type = "l", lty = "dashed", col = "gold")
-    #lines(151:(150+length(forecastsLower2)), forecastsLower2, type = "l", lty = "dashed", col = "gold")
+    lines(151:(150+length(forecasts)), forecasts, type = "l", col = "red")
+    lines(151:(150+length(forecastsUpper)), forecastsUpper, type = "l", lty = "dashed", col = "blue")
+    lines(151:(150+length(forecastsLower)), forecastsLower, type = "l", lty = "dashed", col = "blue")
+    lines(151:(150+length(forecastsUpper2)), forecastsUpper2, type = "l", lty = "dashed", col = "gold")
+    lines(151:(150+length(forecastsLower2)), forecastsLower2, type = "l", lty = "dashed", col = "gold")
     points(anomalies[,1], metricsts[anomalies[,1]], pch = 24, bg = "red")
     
     #Compute outliers' residuals and scores
-    
-    #这里计算的是整个序列的残差标准差，效率极其不高！！！且没办法实现实时计算
     forecastSd <- sd(forecastsResiduals)
     outliersResiduals <- forecasts[outliers-150]-metricsts[outliers]
     outliersScores <- abs(outliersResiduals/forecastSd)
@@ -97,6 +86,7 @@ detection <- function(metrics = rawData, forecastData = NULL, step = 8, windowSi
     points(outliers[orangeResidual], metricsts[outliers[orangeResidual]], pch = 23, bg = "orange")
     points(outliers[redResidual], metricsts[outliers[redResidual]], pch = 23, bg = "red")
     
+    #legend(0.2, 95, c(blueResidual, yellowResidual, orangeResidual, redResidual), col = c(blue, yellow, orange, red))
     results1 <- cbind(metrics[outliers,], ForecastValues = forecasts[outliers-150], Residuals= outliersResiduals, Scores = outliersScores)
     results2 <-anomalies
     
@@ -131,14 +121,6 @@ findAnomalies <- function(outliers, k){
     return(results)
 }
 
-multiMetricsDetection(metricsList){
-    results <- NULL
-    for(n in metricsList){
-        singleResult <- detection(metrics = n，step = 15)
-        results  <- list(singleResult, )
-    }
-}
-
 #Anomalies detection using median absolute deviation
 madDetection<- function(){
     windowSize <- 535
@@ -167,18 +149,4 @@ madDetection<- function(){
     outliers <- c(outliers,which(dm > dmMax & stats < madBase))
     
     outliers
-}
-
-aggt <- function(ts, step){
-    t <- ts[[1]]
-    v <- as.numeric(ts[[2]])
-    i <- 1
-    METRICS_TIME<- NULL
-    VALUE <- NULL
-    while(i < nrow(ts)){
-        VALUE <- c(VALUE, mean(v[i:(i+step-1)], na.rm = TRUE))
-        METRICS_TIME <- c(METRICS_TIME, floor(median(t[i:(i+step-1)], na.rm = TRUE)))
-        i <- i+step
-    }
-    return(data.frame(METRICS_TIME, VALUE, stringsAsFactors = FALSE))
 }
